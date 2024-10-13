acc9b8dab385b94594678fc51bf2070e274c9760
X_kengdada <1547290076@qq.com>
2024-10-13 18:52:13 +0800

    update

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,23 +7,21 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn is_even(num: i32) -> bool {
-    num % 2 == 0
+    num % 2 == 1
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(11));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        assert!(!is_even(10));
     }
 }
